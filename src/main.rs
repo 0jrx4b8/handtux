57e08ebb789d3f78e6d7dd9121d377dbc74d8459
0jrx4b8 <0jrx4b8@proton.me>
2024-11-23 12:59:06 +0100

    kalosm inmplementation

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,48 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use std::collections::HashMap;
 use base64::prelude::BASE64_STANDARD;
 use base64::Engine;
 use std::io::Cursor;
-use image::ImageReader;
-use rusty_tesseract::{Args, Image};
+use image::ImageDecoder;
+use kalosm::vision::*;
+use std::sync::Mutex;
+use tauri::State;
 
 #[tauri::command]
-async fn perform_ocr(data_url: String) -> String {
+async fn perform_ocr(state: State<'_, ModelState>, data_url: String) -> Result<String, String> {
+    let mut model = state.0.lock().map_err(|_| "Err unlocking").unwrap();
+
     let base64_data = data_url.split(',').nth(1).ok_or("Invalid data URL format").unwrap();
     let image_base64_data = BASE64_STANDARD.decode(base64_data).map_err(|_| "Failed to decode image data").unwrap();
-    let image_reader =
-        ImageReader::new(Cursor::new(image_base64_data))
+    let d_image =
+        image::io::Reader::new(Cursor::new(image_base64_data))
             .with_guessed_format()
             .unwrap()
             .decode()
             .unwrap();
-    let image = Image::from_dynamic_image(&image_reader).unwrap();
 
-    let default_args = Args {
-        lang: "osd".to_string() ,
-        config_variables: HashMap::from([(
-            "tessedit_char_whitelist".into(),
-            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@.;?:/!+=-$£€* '".into(),
-        )]),
-        dpi:Some(600),
-        psm:Some(11),
-        oem:Some(1)
-    };
+    let inference_settings = OcrInferenceSettings::new(d_image).unwrap();
 
-    let output = rusty_tesseract::image_to_string(&image, &default_args).unwrap();
+    let output = model
+        .recognize_text(inference_settings)
+        .unwrap();
 
-    println!("The string is: {:?}", output);
+    println!("{}", output);
 
-    output
+    Ok(output)
 }
 
-fn main() {
+struct ModelState(Mutex<Ocr>);
+
+#[tokio::main]
+async fn main() {
+    let model = Ocr::builder().build().await.unwrap();
+
+    let model_state = ModelState(Mutex::new(model));
+
     tauri::Builder::default()
+        .manage(model_state)
         .invoke_handler(tauri::generate_handler![perform_ocr])
         .run(tauri::generate_context!())
         .expect("error while running Tauri application");
